9ee06015ee45bc7e1d881ad82bd1c603e523027e
Mohammad Mustafa Ahmed <M.Ahmed002@umb.edu>
2025-02-10 00:49:28 -0500

    Add ui componenets and prisma schema

---
+++ src/app/sync-user/page.tsx
@@ -0,0 +1,13 @@
+import { db } from '@/server/db'
+import { auth, clerkClient } from '@clerk/nextjs/server'
+import { notFound, redirect } from 'next/navigation'
+
+const SyncUser = async () => {
+  const { userId } = await auth()
+  if (!userId) {
+    throw new Error('User not found')
+  }
+  const client = await clerkClient()
+  
+}
+
